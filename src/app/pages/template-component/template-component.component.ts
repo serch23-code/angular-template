585c11f5412483dcc952913d6f036f6d40c9d7b5
Sergio <szepahua.git@gmail.com>
2020-10-16 20:40:05 -0500

    add prettier tool

--- src/app/pages/template-component/template-component.component.ts
+++ src/app/pages/template-component/template-component.component.ts
@@ -3,18 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-template-component',
   templateUrl: './template-component.component.html',
-  styleUrls: ['./template-component.component.css']
+  styleUrls: ['./template-component.component.css'],
 })
 export class TemplateComponentComponent implements OnInit {
   public nameIwanos = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-    this.nameIwanos = [ {
-      name: 'Carlos Peréz'},
-      {name: 'Julio Alberto Duran'},
-      {name: 'Emanuel'}];
+    this.nameIwanos = [
+      {
+        name: 'Carlos Peréz',
+      },
+      { name: 'Julio Alberto Duran' },
+      { name: 'Emanuel' },
+    ];
   }
-
 }
